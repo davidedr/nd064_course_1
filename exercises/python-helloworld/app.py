6d67fa1dfb8bb0c3ffb93009d029fdf967f5a54a
DavideDR <d.dr@libero.it>
2021-07-01 16:03:42 +0200

    Solves 2.11 Exercise: Endpoints for Application Status

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status():
+    return json.dumps({'result': 'OK - healty'}), 200, {'ContentType':'application/json'} 
+
+@app.route("/metrics")
+def metrics():
+    return json.dumps({'UserCount': 140, 'UserCountActive': 23}), 200, {'ContentType':'application/json'} 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
